2d9fc7fe61df9a37132b1768cf32e1b9e030043d
ThallerN <thaller.n01@htl-ottakring.ac.at>
2017-11-27 16:07:27 +0100

    AAAAAAAAAA

---
+++ aaaaaa/aaaaaa/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aaaaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intVar = 3;
+            var doubleVar = 1.2;
+            var floatVar = 1.2f;
+            var longVar = 15L;
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(short.MinValue);
+            intVar = int.MaxValue;
+            //short s = (short)intVar; // Casting
+            //s = Convert.ToInt16(intVar); // Konvertierung überprüft Überlauf
+            Console.WriteLine(s);
+            var wert1 = s + 3;
+            var wert2 = 5 + 1.2;
+            var wert3 = 4.3f * .5.5;
+
+        }
+    }
+}
